082aec08feb71456a7d58f1e51f91bbe98e6a14c
fadedead <fadedeadd@gmail.com>
2023-05-31 11:58:33 +0530

    Fixed: The divs are now centered

--- sketch.js
+++ sketch.js
@@ -1,42 +1,45 @@
 function sketch() {
-  let page = document.createElement('div');
-  let sketchArea = document.createElement('div');
+  let page = document.createElement("div");
+  let sketchArea = document.createElement("div");
 
   let perCell = {
-    height : "42px",
-    width : "42px",
-    display : "flex",
-   flexShrink : "0", 
-    background : "black",
+    height: "42px",
+    width: "42px",
+    display: "flex",
+    flexShrink: "0",
+    flexDirection: "column",
+    background: "black",
   };
 
-  for(let rSize = 0; rSize < 16; rSize++){
-    let row = document.createElement('div');
-    
-    for(let cSize = 0; cSize < 16; cSize++){
-      let currChild = document.createElement('div');
-      row.appendChild(currChild);
-      Object.assign(currChild.style, perCell);
-    }
+  let rows = [];
+  for (let size = 0; size < 16; size++) {
+    let row = document.createElement("div");
+    rows.push(row);
+    Object.assign(row.style, perCell);
 
     sketchArea.appendChild(row);
-    Object.assign(row.style, perCell);
   }
- 
+  for (let col of rows) {
+    for (let size = 0; size < 16; size++) {
+      let currCol = document.createElement("div");
+      col.appendChild(currCol);
+      Object.assign(currCol.style, perCell);
+    }
+  }
+
   let sketchStyle = {
-    display : "flex",
-    alignItems : "center",
-    justifyContent : "center",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
   };
-  Object.assign(page.style, sketchStyle)
-  page.appendChild(sketchArea); 
+
+  Object.assign(sketchArea.style, sketchStyle);
+  page.appendChild(sketchArea);
   document.body.appendChild(page);
-  
- 
 }
 
 function main() {
- sketch(); 
+  sketch();
 }
 
 main();
