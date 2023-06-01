77c55f64267cdcb76042a41d54dd4da366691060
fadedead <fadedeadd@gmail.com>
2023-06-01 12:47:16 +0530

    Added: part of code which decide the size of each div

--- sketch.js
+++ sketch.js
@@ -2,16 +2,20 @@ function sketch() {
   let page = document.createElement("div");
   let sketchArea = document.createElement("div");
 
+  let currSize = 100;
+  let sizeP = pixelSize(currSize);
   let perCell = {
     display: "flex",
     flexDirection: "column",
-    flexShrink: '0',
-    height : "40px",
-    width : "40px",
+    flexShrink: "0",
+    height: sizeP,
+    width: sizeP,
+    margin: "0",
+    padding: "0",
   };
 
   let rows = [];
-  for (let size = 0; size < 16; size++) {
+  for (let size = 0; size < currSize; size++) {
     let row = document.createElement("div");
     rows.push(row);
     Object.assign(row.style, perCell);
@@ -21,7 +25,7 @@ function sketch() {
 
   let classNames = [];
   for (let col of rows) {
-    for (let size = 0; size < 16; size++) {
+    for (let size = 0; size < currSize; size++) {
       let currCol = document.createElement("div");
       col.appendChild(currCol);
       currCol.classList.add(`hov${size}-${colR}`);
@@ -31,17 +35,25 @@ function sketch() {
     }
     colR += 1;
   }
-  
 
   let sketchStyle = {
     display: "flex",
-    alignItems: "center",
-    justifyContent: "center",
+    height: "640px",
+    width: "640px",
+    display: "flex",
   };
 
   Object.assign(sketchArea.style, sketchStyle);
   page.appendChild(sketchArea);
   document.body.appendChild(page);
+
+  let pageStyle = {
+    display: "flex",
+    flexDirection : "column",
+    alignItems: "center",
+    justifyContent: "center",
+  };
+  Object.assign(page.style, pageStyle);
   return classNames;
 }
 
@@ -51,6 +63,11 @@ function addListenersForHov(classes){
     currDiv[0].addEventListener('mouseover', (event) => currDiv[0].style.background = "black")
   }
 }
+
+function pixelSize(size){
+  return ((640/size).toString()+'px')
+}
+
 function main() {
   let clasess = sketch();
   addListenersForHov(clasess);
